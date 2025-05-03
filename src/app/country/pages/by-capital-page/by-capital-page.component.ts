888ccc7aa277f1ce6706d5a88b54b6b732d59c94
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-05-03 12:44:04 -0500

    feat: agregar servicio CountryService y lógica para búsqueda por capital

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CountrySearchInputComponent } from '../../components/country-search-input/country-search-input.component'; // Importar
 import { CountryListComponent } from '../../components/country-list/country-list.component'; // Importar
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -14,9 +15,16 @@ import { CountryListComponent } from '../../components/country-list/country-list
 export class ByCapitalPageComponent {
   public countries: any[] = []; // Añadir propiedad para la lista de países
 
+  countryService = inject(CountryService); // Inyectar el servicio
+
   // Renombrar y adaptar el método para manejar la búsqueda
   searchByCapital(term: string): void {
     console.log('Buscando por capital:', term);
-    // Aquí iría la lógica para llamar al servicio y actualizar this.countries
+    // Llamar al servicio y suscribirse para obtener la respuesta
+    this.countryService.searchByCapital(term).subscribe((countries) => {
+      console.log('Respuesta del servicio:', countries);
+      // Aquí también actualizarías this.countries si fuera necesario
+      // this.countries = countries;
+    });
   }
 }
